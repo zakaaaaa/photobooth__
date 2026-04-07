45e02bfd461157d5b6c3e8e56d3c861c2995e835
tuesday <aryasetiap.code@gmail.com>
2026-04-07 22:11:11 +0700

    feat: refactor EmailService to utilize ApiService for sending result links

--- lib/services/email_service.dart
+++ lib/services/email_service.dart
@@ -1,38 +1,17 @@
-import 'package:photobooth_app/services/app_logger.dart';
-
-import 'dart:io';
-import 'package:mailer/mailer.dart';
-import 'package:mailer/smtp_server.dart';
+import 'package:photobooth_app/services/api_service.dart';
 
 class EmailService {
-  // ⚠️ GANTI DENGAN EMAIL & APP PASSWORD ANDA ⚠️
-  final String _username = 'zakakurnia0@gmail.com';
-  final String _password = 'sgbo kslt altd hlvl';
+  final ApiService _apiService = ApiService();
 
-  // UBAH PARAMETER KE List<String> filePaths
-  Future<bool> sendEmailWithAttachments(
-      String recipientEmail, List<String> filePaths) async {
-    final smtpServer = gmail(_username, _password);
-
-    final message = Message()
-      ..from = Address(_username, 'ITA Optical Photobooth')
-      ..recipients.add(recipientEmail)
-      ..subject = 'Halo, terimakasih sudah berfoto dengan kami 📸'
-      ..text =
-          'Ayo posting di instagram stories kamu dan tag kami @eyesonkanawa @optik_ita @optik.indojaya 🎉';
-
-    // LOOPING UNTUK MENAMBAHKAN SEMUA FILE
-    for (String path in filePaths) {
-      message.attachments.add(FileAttachment(File(path)));
-    }
-
-    try {
-      final sendReport = await send(message, smtpServer);
-      AppLogger.debug('Message sent: $sendReport');
-      return true;
-    } catch (e) {
-      AppLogger.debug('Message not sent.\n$e');
-      return false;
-    }
+  Future<bool> sendResultLink({
+    required String hwid,
+    required String recipientEmail,
+    required String resultUrl,
+  }) {
+    return _apiService.sendResultEmail(
+      hwid: hwid,
+      recipientEmail: recipientEmail,
+      resultUrl: resultUrl,
+    );
   }
 }
